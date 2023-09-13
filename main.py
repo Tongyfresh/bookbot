a452676cd1f72790e40ac121ed66c13893c2ac1e
Tongyfresh <tip.bridge@gmail.com>
2023-09-13 15:30:49 -0600

    changes to code and .gitignore

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-with open(books/frankenstein.txt) as f
-file_contents = f.read()
-words = 
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    print(text)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+main()
